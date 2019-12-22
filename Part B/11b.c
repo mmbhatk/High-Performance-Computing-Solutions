b95a1488c639b7272f434d0418ac4375223cb707
mmbhatk <kmanasvibhat@gmail.com>
2019-12-23 00:40:36 +0530

    Initial commit.

--- Part B/11b.c
+++ Part B/11b.c
@@ -1,33 +1,27 @@
-#include <stdio.h>
-#include <mpi.h>
+#include<mpi.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(int argc,char *argv[])
+int main(int argc, char* argv[])
 {
-	int i, rank, size, value, sum = 0;
-	int Source, Destination;
+	int i, rank, size, sum, value;
 	MPI_Status status;
 
-	MPI_Init(&argc,&argv);
-	MPI_Comm_size(MPI_COMM_WORLD,&size);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	if(rank == 0)
 	{
-		for(i = 1 ; i < size ; i++)
+		for(i = 1; i < size; i++)
 		{
-			Source = i;
-			MPI_Recv(&value, 1, MPI_INT, Source, 0, MPI_COMM_WORLD, &status);
+			MPI_Recv(&value, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
 			sum = sum + value;
 		}
-		printf("MyRank = %d, SUM = %d\n", rank, sum);
+		printf("\n[%d] Sum = %d\n", rank, sum);
 	}
-
 	else
-	{
-		Destination = 0;
-		MPI_Send(&rank, 1, MPI_INT, Destination, 0, MPI_COMM_WORLD);
-	}
+		MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 
 	MPI_Finalize();
-
 }
--- Part B/practice.c
+++ Part B/practice.c
@@ -4,18 +4,24 @@
 
 int main(int argc, char* argv[])
 {
-	int i, rank, size, msg, data[10];
+	int i, rank, size, sum, value;
+	MPI_Status status;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	msg = rank + 10;
-
-	MPI_Gather(&msg, 1, MPI_INT, data, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
 	if(rank == 0)
+	{
 		for(i = 1; i < size; i++)
-			printf("\nFrom [%d]: %d", i, data[i]);
+		{
+			MPI_Recv(&value, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+			sum = sum + value;
+		}
+		printf("\n[%d] Sum = %d\n", rank, sum);
+	}
+	else
+		MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+
 	MPI_Finalize();
 }
