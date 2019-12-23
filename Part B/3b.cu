04a00859b4d6194d882f033e638908349fd140d6
mmbhatk <kmanasvibhat@gmail.com>
2019-12-23 18:16:13 +0530

    Initial commit.

--- Part B/3b.cu
+++ Part B/3b.cu
@@ -19,31 +19,29 @@ __global__ void SM(float *array)
 	int i, index = threadIdx.x;
 	float average, sum = 0.0;
  
-  __shared__ float shared_arr[128];
+	 __shared__ float shared_arr[128];
 	shared_arr[index] = array[index];
  
 	__syncthreads();
 
-  for(i = 0; i <= index; i++) sum += shared_arr[i];
+  	for(i = 0; i <= index; i++) sum += shared_arr[i];
 
-  average = sum / (index + 1);
+  	average = sum / (index + 1);
 	printf("Thread id = %d\t Average = %f\n", index, average);
 }
 
 int main(int argc, char **argv)
 {
-  float h_arr[128], *d_arr;
-  cudaMalloc((void **) &d_arr, sizeof(float) * 128);
+  	float h_arr[128], *d_arr;
+	cudaMalloc((void **) &d_arr, sizeof(float) * 128);
  
-  LM<<<1, 128>>>(2.0);
+	LM<<<1, 128>>>(2.0);
  
-  cudaMemcpy((void *)d_arr, (void *)h_arr, sizeof(float) * 128, cudaMemcpyHostToDevice);
-  GM<<<1, 128>>>(d_arr);
+	cudaMemcpy((void *)d_arr, (void *)h_arr, sizeof(float) * 128, cudaMemcpyHostToDevice);
+	GM<<<1, 128>>>(d_arr);
   
-  cudaMemcpy((void *)h_arr, (void *)d_arr, sizeof(float) * 128, cudaMemcpyDeviceToHost);
-  SM<<<1, 128>>>(d_arr);
+	cudaMemcpy((void *)h_arr, (void *)d_arr, sizeof(float) * 128, cudaMemcpyDeviceToHost);
+	SM<<<1, 128>>>(d_arr);
 
 	cudaDeviceSynchronize();
- 
-	return 0;
 }
