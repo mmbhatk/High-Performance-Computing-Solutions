61c421c75fcaeae2a1725cee79afd347396212dd
mmbhatk <kmanasvibhat@gmail.com>
2019-12-23 14:39:38 +0530

    Initial commit.

--- Part A/practice.c
+++ Part A/practice.c
@@ -5,31 +5,30 @@
 
 int main()
 {
-	int i, p = 2, count, *prime;
+	int i, p = 2, count = 0, *primes;
 	double t1, t2;
 
-	prime = (int *)malloc((N+1) * sizeof(int));
-	for(i = 0; i < N; i++) prime[i] = 1;
+	primes = (int *)malloc((N+1) * sizeof(int));
+	for(i = 2; i <= N; i++) primes[i] = 1;
 
-	printf("\nMaximum number of threads: %d\n", omp_get_max_threads());
+	printf("\nMaximum number of threads: %d", omp_get_max_threads());
 
 	t1 = omp_get_wtime();
 
 	#pragma omp parallel firstprivate(p) private(i)
 	while(p * p <= N)
 	{
-		if(prime[p] == 1)
+		if(primes[p] == 1)
 			#pragma omp for
-			for(i = p * p; i <= N; i += p) prime[i] = 0;
+			for(i = 2 * p; i <= N; i += p) primes[i] = 0;
 		p += 1;
 	}
 
 	t2 = omp_get_wtime();
-	printf("\nTime taken: %.2f seconds\n", t2 - t1);
+	printf("\nTime taken: %.2f", t2 - t1);
 
 	count = 0;
-	for(i = 2; i < N; i++)
-		if(prime[i]) count++;
-
+	for(i = 2; i <= N; i++)
+		if(primes[i]) count++;
 	printf("\n%d primes between 0 and %d", count, N);
 }
